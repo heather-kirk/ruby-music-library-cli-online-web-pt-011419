b23b060c1897d94171431d30de9da029e26d9546
Heather Kirk <hskirk@gmail.com>
2019-02-26 02:46:37 +0000

    Automatically backed up by Learn

--- lib/models/Song.rb
+++ lib/models/Song.rb
@@ -42,9 +42,7 @@ class Song
    @@all.detect {|artist| artist.name == name } 
  end 
  
-
- 
- def self.find_or_create_by_name(name)
+  def self.find_or_create_by_name(name)
    self.find_by_name(name) || self.create(name)
  end 
  
